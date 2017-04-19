8e89c35f1540d221d2bd83aa2f2df8c3b60b7a34
MirekSz <miro1994>
2017-04-19 18:59:34 +0200

    fix

--- src/test/java/org/hibernate/bugs/ORMStandaloneTestCase.java
+++ src/test/java/org/hibernate/bugs/ORMStandaloneTestCase.java
@@ -1,15 +1,24 @@
 package org.hibernate.bugs;
 
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.bugs.model.Product;
-import org.hibernate.bugs.model.SaleDocument;
-import org.hibernate.bugs.model.SaleDocumentItem;
-import org.hibernate.bugs.model.SaleDocumentSummary;
 import org.hibernate.cfg.Environment;
 import org.hibernate.testing.TestForIssue;
 import org.junit.Before;
@@ -47,27 +56,256 @@ public class ORMStandaloneTestCase {
 		Transaction beginTransaction = session.beginTransaction();
 
 		SaleDocumentItem saleDocumentItem = new SaleDocumentItem();
-		session.save(saleDocumentItem);
-
-		Product product = new Product();
-		session.save(product);
-
-		SaleDocument saleDocument = new SaleDocument();
-		session.save(saleDocument);
-
-		SaleDocument correction = new SaleDocument();
-		session.save(correction);
-		saleDocument.setCorerctionSubject(correction);
+		session.persist(saleDocumentItem);
 
 		SaleDocumentSummary saleDocumentsummary = new SaleDocumentSummary();
-		session.save(saleDocumentsummary);
+		session.persist(saleDocumentsummary);
 
+		saleDocumentsummary.addItem(saleDocumentItem);
+
+		Product product = new Product();
+		session.persist(product);
 		saleDocumentItem.setProduct(product);
-		saleDocumentItem.setSaleDocument(saleDocument);
-		saleDocumentItem.setSummary(saleDocumentsummary);
+
+		SaleDocument saleDocument = new SaleDocument();
+		session.persist(saleDocument);
+		saleDocument.addItem(saleDocumentItem);
+
+		SaleDocument correction = new SaleDocument();
+		session.persist(correction);
+
+		saleDocument.setCorerctionSubject(correction);
 
 		beginTransaction.commit(); // PRIMARY_KEY VIOLATION, set
 									// hibernate.order_inserts = false and works
+	}
+
+	@Entity(name = "Product")
+	public static class Product {
+
+		@Id
+		@GeneratedValue
+		private Long id;
+		@Column(unique = true)
+		private String name;
+		private String description;
+		private Integer quantity;
+		private BigDecimal price;
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getDescription() {
+			return description;
+		}
+
+		public void setDescription(String description) {
+			this.description = description;
+		}
+
+		public BigDecimal getPrice() {
+			return price;
+		}
+
+		public void setPrice(BigDecimal price) {
+			this.price = price;
+		}
+
+		public Integer getQuantity() {
+			return quantity;
+		}
+
+		public void setQuantity(Integer quantity) {
+			this.quantity = quantity;
+		}
+	}
+
+	@Entity(name = "SaleDocument")
+	public static class SaleDocument {
+
+		@Id
+		@GeneratedValue
+		private Long id;
+		private String number;
+		@OneToMany(fetch = FetchType.LAZY, mappedBy = "saleDocument")
+		private Set<SaleDocumentItem> items = new HashSet();
+		@JoinColumn(name = "ID_SALE_DOCUMENT_CORRECTION", nullable = true)
+		@ManyToOne(fetch = FetchType.LAZY)
+		private SaleDocument corerctionSubject;
+		private BigDecimal totalPrice;
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getNumber() {
+			return number;
+		}
+
+		public void setNumber(String number) {
+			this.number = number;
+		}
+
+		public Set<SaleDocumentItem> getItems() {
+			return items;
+		}
+
+		public void setItems(Set<SaleDocumentItem> items) {
+			this.items = items;
+		}
+
+		public BigDecimal getTotalPrice() {
+			return totalPrice;
+		}
+
+		public void setTotalPrice(BigDecimal totalPrice) {
+			this.totalPrice = totalPrice;
+		}
+
+		public void addItem(SaleDocumentItem sdi) {
+			this.getItems().add(sdi);
+			sdi.setSaleDocument(this);
+		}
+
+		public SaleDocument getCorerctionSubject() {
+			return corerctionSubject;
+		}
+
+		public void setCorerctionSubject(SaleDocument corerctionSubject) {
+			this.corerctionSubject = corerctionSubject;
+		}
+	}
+
+	@Entity(name = "SaleDocumentItem")
+	public class SaleDocumentItem {
+
+		@Id
+		@GeneratedValue
+		private Long id;
+		private Integer lp;
+		@ManyToOne(optional = true)
+		private Product product;
+		@JoinColumn(name = "ID_SALE_DOCUMENT", nullable = true)
+		@ManyToOne(fetch = FetchType.LAZY)
+		private SaleDocument saleDocument;
+		@JoinColumn(name = "ID_SALE_DOCUMENT_SUMAMRY", nullable = true)
+		@ManyToOne(fetch = FetchType.LAZY)
+		private SaleDocumentSummary summary;
+		private Integer quantity;
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public Integer getLp() {
+			return lp;
+		}
+
+		public void setLp(Integer lp) {
+			this.lp = lp;
+		}
+
+		public Product getProduct() {
+			return product;
+		}
+
+		public void setProduct(Product product) {
+			this.product = product;
+		}
+
+		public Integer getQuantity() {
+			return quantity;
+		}
+
+		public void setQuantity(Integer quantity) {
+			this.quantity = quantity;
+		}
+
+		public SaleDocument getSaleDocument() {
+			return saleDocument;
+		}
+
+		public void setSaleDocument(SaleDocument saleDocument) {
+			this.saleDocument = saleDocument;
+		}
+
+		public SaleDocumentSummary getSummary() {
+			return summary;
+		}
+
+		public void setSummary(SaleDocumentSummary summary) {
+			this.summary = summary;
+		}
+	}
+
+	@Entity(name = "SaleDocumentSummary")
+	public class SaleDocumentSummary {
+
+		@Id
+		@GeneratedValue
+		private Long id;
+		private String number;
+		@OneToMany(fetch = FetchType.LAZY, mappedBy = "summary")
+		private Set<SaleDocumentItem> items = new HashSet();
+		private BigDecimal totalPrice;
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getNumber() {
+			return number;
+		}
+
+		public void setNumber(String number) {
+			this.number = number;
+		}
+
+		public Set<SaleDocumentItem> getItems() {
+			return items;
+		}
+
+		public void setItems(Set<SaleDocumentItem> items) {
+			this.items = items;
+		}
+
+		public BigDecimal getTotalPrice() {
+			return totalPrice;
+		}
+
+		public void setTotalPrice(BigDecimal totalPrice) {
+			this.totalPrice = totalPrice;
+		}
+
+		public void addItem(SaleDocumentItem sdi) {
+			this.getItems().add(sdi);
+			sdi.setSummary(this);
+		}
 	}
 
 }
